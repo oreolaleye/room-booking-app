aad69626d9cd4a81847d3d96f90457fbdd465ccd
Ore <olaleyeoreoluwa1@gmail.com>
2022-03-09 03:40:24 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,5 +27,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running at port ${port}`));
